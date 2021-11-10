b8ad418603a2ab1c11a0d6cae8d315bf41ef4e00
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-10 22:17:08 +0000

    Set add button and inputs information

---
+++ js/addInfo.js
@@ -0,0 +1,7 @@
+//  File to add the information on the input
+document.addEventListener('DOMContentLoaded', () => {
+    const addBtn = document.getElementById("addBtn");   // Add button
+    // Inputs information
+    const session = document.getElementById("sessionInput").value;    // Session
+    const password = document.getElementById("pwdInput").value; // Password
+})
