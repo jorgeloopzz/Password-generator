54a76af3d766fbfdc7449b7b28460f29209e5a99
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-11 22:18:52 +0000

    Removed <> characters to avoid bugs

--- js/generatePwd.js
+++ js/generatePwd.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const characters = [
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", ">", "$", "%", "/", "(", ")", "!", "*", "ç", "ñ", "!", "ª"
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "$", "%", "/", "(", ")", "!", "*", "ç", "ñ", "!", "ª"
     ]
     const generateBtn = document.getElementById("generatePwd"); //Generate password button
     const pwdField = document.getElementById("pwdInput");   // Field where to put the password
