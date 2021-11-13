c04273c25adec5dfe0e2efdd586df0b22d1f822f
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-13 01:19:32 +0000

    Added a for loop to avoid every sentence get showed

--- js/addInfo.js
+++ js/addInfo.js
@@ -10,12 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //  Add the information
     addBtn.addEventListener('click', () => {
-        if (session.value == "" & password.value == "") {
-            errors[2].style.display = "inline"
-        } else if (password.value == "") {
-            errors[1].style.display = "inline"
-        } else if (session.value == "") {
-            errors[0].style.display = "inline"
+        for (let i = 0; i < errors.length; i++) {
+            errors[i].style.display = "none"
+            if (session.value == "" & password.value == "") {
+                errors[2].style.display = "inline"
+            } else if (password.value == "") {
+                errors[1].style.display = "inline"
+            } else if (session.value == "") {
+                errors[0].style.display = "inline"
+            }
         }
         sessionTd.innerHTML = session.value;
         passwordTd.innerHTML = password.value;
