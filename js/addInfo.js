faa06540a2f0b670a605c7612caf52cbc4fc4c5d
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-13 01:06:02 +0000

    Changed variables of error messages for a queryselectorAll

--- js/addInfo.js
+++ js/addInfo.js
@@ -4,10 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Inputs information
     const session = document.getElementById("sessionInput");    // Session
     const password = document.getElementById("pwdInput"); // Password
-    //  Errors messages
-    const sessionError = document.getElementById("sessionError");   // Session input empty
-    const pwdError = document.getElementById("pwdError");   // Password imput empty
-    const superError = document.getElementById("superError");   // Both inputs empty
+    const errors = document.querySelectorAll("h3"); // Errors messages
     const sessionTd = document.getElementById("try1");
     const passwordTd = document.getElementById("try2");
 
@@ -17,5 +14,4 @@ document.addEventListener('DOMContentLoaded', () => {
         passwordTd.innerHTML = password.value;
     })
 
-    //  Manage errors
 })
