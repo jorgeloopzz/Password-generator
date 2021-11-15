eabed418464c7f48a6c90e0ae5b008cd4adaf5c0
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-15 12:42:52 +0000

    Removed for

--- js/addInfo.js
+++ js/addInfo.js
@@ -1,28 +1,21 @@
 //  File to add the information on the input
 document.addEventListener('DOMContentLoaded', () => {
-    const addBtn = document.getElementById("addBtn");   // Add button
+    const addBtn = document.getElementById('addBtn');   // Add button
     // Inputs information
-    const session = document.getElementById("sessionInput");    // Session
-    const password = document.getElementById("pwdInput"); // Password
-    const errors = document.querySelectorAll("h3"); // Errors messages
-    const sessionTd = document.getElementById("try1");
-    const passwordTd = document.getElementById("try2");
-
+    const session = document.getElementById('sessionInput');    // Session
+    const password = document.getElementById('pwdInput'); // Password
+    // Errors messages
+    const error = document.querySelector('h3');
+    // Table
+    const table = document.getElementById('table');   
+    
     //  Add the information
     addBtn.addEventListener('click', () => {
         // Errors
-        for (let i = 0; i < errors.length; i++) {
-            errors[i].style.display = "none"  // Loop through every sentence to keep them in display none
-            if (session.value == "" & password.value == "") {
-                errors[2].style.display = "inline"  // Display inline for showing the sentence and the icon in horizontal
-            } else if (password.value == "") {
-                errors[1].style.display = "inline"
-            } else if (session.value == "") {
-                errors[0].style.display = "inline"
-            } else {
-                sessionTd.innerHTML = session.value;
-                passwordTd.innerHTML = password.value;
-            }
+        if ( session.value == '' || password.value == '' ) {
+            error.style.display = 'inline'
+        } else {
+            error.style.display = 'none'
         }
     })
 
