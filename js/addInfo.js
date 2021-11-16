820eb9003af63c62e269b99ef7630f883c9de4a1
Jorgedeveloopzz <jorgeloopzz@protonmail.com>
2021-11-16 21:03:02 +0000

    Mada function to remove passwords

--- js/addInfo.js
+++ js/addInfo.js
@@ -4,17 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     // Inputs information
     const session = document.getElementById('sessionInput');    // Session
     const password = document.getElementById('pwdInput'); // Password
-    // Errors messages
+    // Error messag
     const error = document.querySelector('h3');
     // Table
-    const table = document.getElementById('table');   
+    const table = document.getElementById('table');
+    let id = 0;  
     
     //  Add the information
     addBtn.addEventListener('click', () => {
+        // Function to remove the table
+        function removeTable(id) {
+            document.getElementById(id).remove();
+        }
+
         // Function to create the table
         function createTable() {
             const tbody = table.getElementsByTagName('tbody')[0]  // Put a [0] to select the tbody element and not the object
             const row = tbody.insertRow()
+            row.setAttribute('id', id++)
             row.innerHTML = `
             <td> ${session.value} </td> <td> ${password.value} </td>
             <td class='btnTd'>
@@ -23,11 +30,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 </button>
             </td>
             `
+            // Make the remove button apart
             const removeBtn = document.createElement('button');
             removeBtn.classList.add('button');
             removeBtn.setAttribute('id', 'removeBtn');
             removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
-            row.children[2].appendChild(removeBtn);
+            row.children[2].appendChild(removeBtn); // Add the button on the 3 column of the table
+            removeBtn.onclick = () => {
+                removeTable(row.getAttribute('id'))
+            }
         }
         // Errors
         if ( session.value == '' || password.value == '' ) {
