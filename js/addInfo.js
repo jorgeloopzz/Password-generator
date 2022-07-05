d54808a212e26e63794708583fc11d11a2d2c2a1
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-07-05 20:24:29 +0100

    Removed functions of the table

--- js/addInfo.js
+++ js/addInfo.js
@@ -11,34 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //  Add the information
   copyBtn.addEventListener("click", () => {
-    // Function to remove the table
-    function removeTable(id) {
-      document.getElementById(id).remove();
-    }
-
-    // Function to create the table
-    function createTable() {
-      const tbody = table.getElementsByTagName("tbody")[0]; // Put a [0] to select the tbody element and not the object
-      const row = tbody.insertRow();
-      row.setAttribute("id", id++);
-      row.innerHTML = `
-            <td> ${session.value} </td> <td> ${password.value} </td>
-            <td class='btnTd'>
-                <button class="button" id="editBtn">
-                    <i class="fas fa-pencil-alt"></i>
-                </button>
-            </td>
-            `;
-      // Make the remove button apart
-      const removeBtn = document.createElement("button");
-      removeBtn.classList.add("button");
-      removeBtn.setAttribute("id", "removeBtn");
-      removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
-      row.children[2].appendChild(removeBtn); // Add the button on the 3 column of the table
-      removeBtn.onclick = () => {
-        removeTable(row.getAttribute("id"));
-      };
-    }
     // Errors
     if (session.value == "" || password.value == "") {
       error.style.visibility = "visible";
