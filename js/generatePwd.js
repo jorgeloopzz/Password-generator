fcc4298a8ce88ab943e1f4ce28a87eb5114d53db
jorgeloopzz <jorgeloopzz@protonmail.com>
2022-08-21 20:17:05 +0100

    Set characters in a string instead of array

--- js/generatePwd.js
+++ js/generatePwd.js
@@ -6,11 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const boxError = document.getElementById("error2"); // Error message
 
   //  Characters to put in the password
-  const characters = [
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "$", "%", "/", "(", ")", "!", "*", "ç", "ñ", "!", "ª"
-  ];
+  const characters = "abcdefghijklmñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTU0123456789$#~%&/()?¿"
 
   //  Function to generate the password
   function generatePassword(array) {
